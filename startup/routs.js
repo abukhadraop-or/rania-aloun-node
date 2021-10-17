bbeb9861e4d3832a5dd963ca4fbea66a8eaa81ee
Rania Aloun <rania_aloun@outlook.com>
2021-10-17 10:19:33 +0300

    removed unnecessary route

--- startup/routs.js
+++ startup/routs.js
@@ -4,7 +4,6 @@ const addTag = require('../routes/addTags');
 const addAssociation = require('../routes/addAssociation');
 const getArticles = require('../routes/getArticles');
 const getTags = require('../routes/getTags');
-const getFilteredArticles = require('../routes/getFilteredArticles');
 const updateArticleLikes = require('../routes/updateArticleLikes');
 const addArticleDirectly = require('../routes/addArticleDirectly');
 
@@ -16,7 +15,6 @@ module.exports = function (app) {
   app.use('/addAssociation', addAssociation);
   app.use('/getArticles', getArticles);
   app.use('/getTags', getTags);
-  app.use('/getFilteredArticles', getFilteredArticles);
   app.use('/updateArticleLikes', updateArticleLikes);
   app.use('/test', addArticleDirectly);
 };
