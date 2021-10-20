b33b5fef4b31dfeec3642f38d1f92bfe8a1ed7a0
Rania Aloun <rania_aloun@outlook.com>
2021-10-20 16:11:46 +0300

    Restructured files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-require('./startup/server')(app);
+require('./src/startup/server')(app);
 
 const PORT = process.env.PORT || 3600;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- src/startup/server.js
+++ src/startup/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const addAssociation = require('../routes/addAssociation');
 const articles = require('../routes/articles');
 const tags = require('../routes/tags');
 
@@ -7,5 +6,4 @@ module.exports = function (app) {
   app.use(express.json());
   app.use('/api/articles', articles);
   app.use('/api/tags', tags);
-  app.use('/addAssociation', addAssociation);
 };
